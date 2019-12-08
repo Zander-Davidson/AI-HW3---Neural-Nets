52fbf4e58de0c4ac5a262d297bcf7739f5f42144
Zander-Davidson <alexander.davidson98@gmail.com>
2019-12-08 14:01:19 -0600

    xor with manually-entered wts/biases

---
+++ xor.py
@@ -0,0 +1,66 @@
+import math
+
+X = [[0,0,0], [0,1,1], [1,0,1], [1,1,0]] # training set, an XOR truth table
+N = 3 # number of layers in neural net
+neurons = [2, 2, 1] # number of neurons in the hidden and output layers, respectively
+
+
+weight = [[[20, 20], [-20, -20]], [[20, 20]]]
+bias = [[-10, 30], [-30]]
+
+
+def sigmoid(a):
+    return 1 / (1 + math.exp(-a))
+
+# weights = [] # [layer][neuron][weight], stores the weights of all neural connections
+# biases = [] # [layer][neuron], stores biases for each nueron
+
+
+
+
+
+# # assign random initial weights
+
+# # output layer has no connection to another
+# for l in range(N-1):
+
+#     # 1 axon per neuron -> "neurons" gives number of axons per layer
+#     for n in range(neurons[l]):
+#         bias[l][n] = 0
+
+#         # if there are h neurons in layer n+1, we need h connections from the current axon to it
+#         for w in range(neurons[l+1]):
+#             weight[l][n][w] = 0
+
+# samples in training set
+for x in X:
+    print('Training set ')
+
+    xIn = x
+    yOut = []
+    
+    # loop through layers, excluding input layer
+    for l in range(1, N):
+        print('\tLayer ' + str(l))
+
+        # loop through neurons in each layer
+        for n in range(neurons[l]):
+            print('\t\tNeuron ' + str(n) + ': ' + str(bias[l-1][n]))
+
+            sum = 0
+
+            # loop through weights for a given neuron in layer j
+            for w in range(neurons[l-1]):
+                print('\t\t\tWeight ' + str(w) + ': ' + str(weight[l-1][n][w]))
+
+                sum += weight[l-1][n][w] * xIn[w]
+
+            yOut.append(sigmoid(sum + bias[l-1][n]))
+
+        print('\txIn = yOut: ' + str(yOut[-1]))
+        xIn = yOut
+
+
+
+
+    
